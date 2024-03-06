f02c01d55b22757cceebfe069286674fdfbd1024
Seif Technone <154022392+SeifTechnone@users.noreply.github.com>
2024-03-06 20:20:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
+curl -o $HOME/.termux/termux-login.py https://raw.githubusercontent.com/SeifTechnone/termux-login/main/termux-login.py
 
+curl -o $PREFIX/bin/termux-user-lock https://raw.githubusercontent.com/SeifTechnone/termux-login/main/termux-user-lock
+
+curl -o $PREFIX/bin/termux-user-unlock https://raw.githubusercontent.com/SeifTechnone/termux-login/main/termux-user-unlock
+
+
+chmod +x $HOME/.termux/termux-login.py
+chmod +x $PREFIX/bin/termux-user-lock
+chmod +x $PREFIX/bin/termux-user-unlock
